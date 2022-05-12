21ed6711de2a1a284e85aec7403332c3b0f40f29
Jose <ze.carlos98@hotmail.com>
2022-05-11 21:00:57 -0300

    commit

---
+++ todo/src/Models/tarefaLista.ts
@@ -0,0 +1,14 @@
+import { Tarefa } from "./tarefa";
+
+export class TarefasLista {
+    constructor(public usuario: string, private listaTarefas: Tarefa[] = []) {
+
+    }
+    get tarefas(): readonly Tarefa[] {
+        return this.listaTarefas;
+    }
+    addTarefa(nomeTarefa: string) {
+        this.listaTarefas.push(new Tarefa(nomeTarefa));
+    }
+
+}
---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import { Tarefa } from "src/Models/tarefa";
+import { TarefasLista } from "src/Models/tarefaLista";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  private list = new TarefasLista("Senac", [
+    new Tarefa("Estudar HTML"),
+    new Tarefa("Estudar CSS"),
+    new Tarefa ("Estudar JAVASCRIPT"),
+    ]);
+    get username(): string {
+      return this.list.usuario; 
+     }
+     get itemCount(): number {
+       return this.list.tarefas.filter(item=> !item.completo).length;  
+      }
+      get items(): readonly Tarefa[] {
+        return this.list.tarefas.filter(item =>!item.completo);
+      }
+      
+      addTarefa(newItem: string) {
+        if (newItem != "") {
+          this.list.addTarefa(newItem);
+        }
+      }
+      showComplete: boolean = false;
+
+    }
